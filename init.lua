87706026f776f4425d2ae57a74fa5d80bbffdb3a
Drin Racaj <drin.racaj@hes-so.ch>
2025-03-03 21:23:23 +0100

    enabled clangd

--- init.lua
+++ init.lua
@@ -15,6 +15,11 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=4'
+vim.cmd 'set softtabstop=4'
+vim.cmd 'set shiftwidth=4'
+
 require 'options'
 
 require 'keymaps'
